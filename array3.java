d1dff3356b4d84f1c85b6dab9f441a49ce8c4ca1
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-18 20:44:54 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,33 @@
+public class array3
+{
+    public static void main(String[] args)
+    {
+        //int data type
+        int[] a = new int[4];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+
+        for (int i = 0; i < 4; i++) {
+
+            System.out.println(a[i]);
+        }
+        System.out.println();
+        //char data type
+        char[] b = new char[3];
+        b[0] = 'a';
+        b[1] = 'b';
+        b[2] = 'c';
+        for (int j = 0; j < 1; j++)
+        {
+        System.out.println(b);
+    }
+        System.out.println();
+        //boolean data type
+        boolean[] c = new boolean[2];
+        c[0] = true;
+        c[1] = false;
+        System.out.println(c[1]);
+    }
+}
